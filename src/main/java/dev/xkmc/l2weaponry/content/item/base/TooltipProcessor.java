f0d9e2e902a21926c3453863e1a28a2f324a1457
lcy0x1 <arthurlcy0x1@gmail.com>
2023-06-18 15:22:22 -0700

    fix all

---
+++ src/main/java/dev/xkmc/l2weaponry/content/item/base/TooltipProcessor.java
@@ -0,0 +1,37 @@
+package dev.xkmc.l2weaponry.content.item.base;
+
+import com.google.common.collect.Multimap;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraftforge.common.ForgeMod;
+
+public class TooltipProcessor {
+
+	public static void processTooltip(Multimap<Attribute, AttributeModifier> ans) {
+		sumAddition(ans, ForgeMod.ATTACK_RANGE.get());
+		sumAddition(ans, ForgeMod.REACH_DISTANCE.get());
+	}
+
+	private static void sumAddition(Multimap<Attribute, AttributeModifier> ans, Attribute attr) {
+		var list = ans.get(attr);
+		AttributeModifier mod = null;
+		double add = 0;
+		for (var e : list) {
+			if (e.getOperation() == AttributeModifier.Operation.ADDITION) {
+				if (mod == null) {
+					mod = e;
+				} else {
+					add += e.getAmount();
+					ans.remove(attr, e);
+				}
+			}
+		}
+		if (add != 0) {
+			ans.remove(attr, mod);
+			add += mod.getAmount();
+			ans.put(attr, new AttributeModifier(mod.getId(), mod.getName(), add, AttributeModifier.Operation.ADDITION));
+		}
+	}
+
+
+}
