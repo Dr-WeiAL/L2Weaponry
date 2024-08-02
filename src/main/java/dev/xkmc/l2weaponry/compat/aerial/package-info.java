77ac7cbe81af6ce3209c601047e5aace105f857f
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-08-02 12:02:49 +0800

    remove compat

--- src/main/java/dev/xkmc/l2weaponry/compat/aerial/package-info.java
+++
@@ -1,8 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-
-package dev.xkmc.l2weaponry.compat.aerial;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
--- src/main/java/dev/xkmc/l2weaponry/compat/undergarden/package-info.java
+++
@@ -1,8 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-
-package dev.xkmc.l2weaponry.compat.undergarden;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
