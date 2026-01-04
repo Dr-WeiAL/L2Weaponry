2f752f75f9ec561cefbabbe8a0e1c9e436d82a23
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-01-04 16:45:09 +0800

    wrap iafce compat

--- src/main/java/dev/xkmc/l2weaponry/compat/dragons/IAFProxyCE.java
+++ src/main/java/dev/xkmc/l2weaponry/compat/dragons/IAFProxyCE.java
@@ -29,82 +29,103 @@ public class IAFProxyCE implements IAFProxy {
 
 	@Override
 	public void fireHit(ItemStack stack, LivingEntity target, LivingEntity user) {
-		if (IafCommonConfig.INSTANCE.tools.dragonFireAbility.getValue()) {
-			if (target instanceof IceDragonEntity) {
-				target.hurt(user.level().damageSources().inFire(), 13.5F);
+		try {
+			if (IafCommonConfig.INSTANCE.tools.dragonFireAbility.getValue()) {
+				if (target instanceof IceDragonEntity) {
+					target.hurt(user.level().damageSources().inFire(), 13.5F);
+				}
+				target.setRemainingFireTicks(100);
+				target.knockback(1.0, user.getX() - target.getX(), user.getZ() - target.getZ());
 			}
-			target.setRemainingFireTicks(100);
-			target.knockback(1.0, user.getX() - target.getX(), user.getZ() - target.getZ());
-		}
+		} catch (Throwable ignore) {
 
+		}
 	}
 
 	@Override
 	public void fireDesc(ItemStack stack, List<Component> list) {
-		list.add(Component.translatable("dragon_sword_fire.hurt1").withStyle(ChatFormatting.GREEN));
-		if (IafCommonConfig.INSTANCE.tools.dragonFireAbility.getValue()) {
-			list.add(Component.translatable("dragon_sword_fire.hurt2").withStyle(ChatFormatting.DARK_RED));
+		try {
+			list.add(Component.translatable("dragon_sword_fire.hurt1").withStyle(ChatFormatting.GREEN));
+			if (IafCommonConfig.INSTANCE.tools.dragonFireAbility.getValue()) {
+				list.add(Component.translatable("dragon_sword_fire.hurt2").withStyle(ChatFormatting.DARK_RED));
+			}
+		} catch (Throwable ignore) {
+
 		}
 	}
 
 	@Override
 	public void iceHit(ItemStack stack, LivingEntity target, LivingEntity user) {
-
-		if (IafCommonConfig.INSTANCE.tools.dragonIceAbility.getValue()) {
-			if (target instanceof FireDragonEntity) {
-				target.hurt(user.level().damageSources().drown(), 13.5F);
+		try {
+			if (IafCommonConfig.INSTANCE.tools.dragonIceAbility.getValue()) {
+				if (target instanceof FireDragonEntity) {
+					target.hurt(user.level().damageSources().drown(), 13.5F);
+				}
+				FrozenData data = FrozenData.get(target);
+				data.setFrozen(target, 200);
+				target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 100, 2));
+				target.addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 100, 2));
+				target.knockback(1.0, user.getX() - target.getX(), user.getZ() - target.getZ());
 			}
+		} catch (Throwable ignore) {
 
-			FrozenData data = FrozenData.get(target);
-			data.setFrozen(target, 200);
-			target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 100, 2));
-			target.addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 100, 2));
-			target.knockback(1.0, user.getX() - target.getX(), user.getZ() - target.getZ());
 		}
 	}
 
 	@Override
 	public void iceDesc(ItemStack stack, List<Component> list) {
-		list.add(Component.translatable("dragon_sword_ice.hurt1").withStyle(ChatFormatting.GREEN));
-		if (IafCommonConfig.INSTANCE.tools.dragonIceAbility.getValue()) {
-			list.add(Component.translatable("dragon_sword_ice.hurt2").withStyle(ChatFormatting.AQUA));
+		try {
+			list.add(Component.translatable("dragon_sword_ice.hurt1").withStyle(ChatFormatting.GREEN));
+			if (IafCommonConfig.INSTANCE.tools.dragonIceAbility.getValue()) {
+				list.add(Component.translatable("dragon_sword_ice.hurt2").withStyle(ChatFormatting.AQUA));
+			}
+		} catch (Throwable ignore) {
+
 		}
 	}
 
 	@Override
 	public void lightningHit(ItemStack stack, LivingEntity target, LivingEntity user) {
-		if (IafCommonConfig.INSTANCE.tools.dragonLightningAbility.getValue()) {
-			boolean flag = true;
-			if (user instanceof Player && (double) user.attackAnim > 0.2) {
-				flag = false;
-			}
-
-			if (!user.level().isClientSide && flag) {
-				LightningBolt e = EntityType.LIGHTNING_BOLT.create(target.level());
-				assert e != null;
-				e.getTags().add("iceandfire.bolt_skip_loot");
-				e.getTags().add(user.getStringUUID());
-				e.addTag(FlagMarker.LIGHTNING);
-				e.moveTo(target.position());
-				if (!target.level().isClientSide) {
-					target.level().addFreshEntity(e);
+		try {
+			if (IafCommonConfig.INSTANCE.tools.dragonLightningAbility.getValue()) {
+				boolean flag = true;
+				if (user instanceof Player && (double) user.attackAnim > 0.2) {
+					flag = false;
 				}
-			}
 
-			if (target instanceof FireDragonEntity || target instanceof IceDragonEntity) {
-				target.hurt(user.level().damageSources().lightningBolt(), 9.5F);
-			}
+				if (!user.level().isClientSide && flag) {
+					LightningBolt e = EntityType.LIGHTNING_BOLT.create(target.level());
+					assert e != null;
+					e.getTags().add("iceandfire.bolt_skip_loot");
+					e.getTags().add(user.getStringUUID());
+					e.addTag(FlagMarker.LIGHTNING);
+					e.moveTo(target.position());
+					if (!target.level().isClientSide) {
+						target.level().addFreshEntity(e);
+					}
+				}
 
-			target.knockback(1.0, user.getX() - target.getX(), user.getZ() - target.getZ());
+				if (target instanceof FireDragonEntity || target instanceof IceDragonEntity) {
+					target.hurt(user.level().damageSources().lightningBolt(), 9.5F);
+				}
+
+				target.knockback(1.0, user.getX() - target.getX(), user.getZ() - target.getZ());
+			}
+		} catch (Throwable ignore) {
+
 		}
 
 	}
 
 	@Override
 	public void lightningDesc(ItemStack stack, List<Component> list) {
-		list.add(Component.translatable("dragon_sword_lightning.hurt1").withStyle(ChatFormatting.GREEN));
-		if (IafCommonConfig.INSTANCE.tools.dragonLightningAbility.getValue()) {
-			list.add(Component.translatable("dragon_sword_lightning.hurt2").withStyle(ChatFormatting.DARK_PURPLE));
+		try {
+			list.add(Component.translatable("dragon_sword_lightning.hurt1").withStyle(ChatFormatting.GREEN));
+			if (IafCommonConfig.INSTANCE.tools.dragonLightningAbility.getValue()) {
+				list.add(Component.translatable("dragon_sword_lightning.hurt2").withStyle(ChatFormatting.DARK_PURPLE));
+			}
+		} catch (Throwable ignore) {
+
 		}
 	}
 
